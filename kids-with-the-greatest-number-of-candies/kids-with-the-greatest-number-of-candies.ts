3ca1de97cc65a4b4945711d8a8912627dcc05af9
Matt Jennings <matt@mattjennings.net>
2024-09-30 12:12:51 -0700

    learning about greatest kids with greatest number of candies algo

---
+++ kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -0,0 +1,56 @@
+/*
+# Kids With the Greatest Number of Candies
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/
+
+## Starting array and extra candies
+*/
+const arr: Array<number> = [2, 3, 5, 1, 3];
+const extraCandies: number = 3;
+
+/**
+## Big O Notion for Algo Below
+- Time complexity: O(n)
+- Space complexity: O(n)
+
+## Runtime per leetcode.com
+67 ms 
+ */
+function kidsWithCandies1(candies: number[], extraCandies: number): boolean[] {
+ /*
+ ## Complexity for line below:
+ - Time: O(n)
+ - Space: O(1)
+ */
+ const maxNumber: number = Math.max(...candies);
+ /*
+ ## Complexity for line below:
+ - Time: O(n)
+ - Space: O(n)
+ */ 
+ return candies.map(num => num + extraCandies >= maxNumber );
+}
+
+console.log(kidsWithCandies1(arr, extraCandies));
+
+/**
+## Big O Notion for Algo Below
+- Time complexity: O(n^2)
+  - Details: For the nested `map` and `every` loops
+- Space complexity: O(n)
+  - For the result array
+
+## Runtime per leetcode.com
+67 ms 
+ */
+function kidsWithCandies2(candies: number[], extraCandies: number): boolean[] {
+ return candies.map((candy, i) => {
+  return candies.every((childCandies, j) => {
+   if (i === j) return true;
+
+   return candy + extraCandies >= childCandies;
+  })
+ });
+}
+
+console.log(kidsWithCandies2(arr, extraCandies));
+
